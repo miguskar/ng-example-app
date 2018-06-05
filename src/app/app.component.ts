75cc6514428aad2b419e41c351c620c93527fc97
Mikael Karlsson <miguskar@gmail.com>
2018-06-05 16:32:21 +0200

    feat(app): show alert on start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   constructor() {}
 
   ngOnInit() {
+    Swal('Tada! 🎉', 'Great success!', 'success');
   }
 }
